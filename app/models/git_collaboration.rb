20e5de91e33491aa8bdb5811c2136e8182ae64f9
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-07-21 11:20:16 +1000

    define user and git collab post relationships

--- app/models/git_collaboration.rb
+++ app/models/git_collaboration.rb
@@ -1,2 +1,3 @@
 class GitCollaboration < ApplicationRecord
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :posts
 
     def to_token_payload
         {
