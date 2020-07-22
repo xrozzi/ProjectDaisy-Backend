dd88d2b763cbab331cd261d3d27935d6de705bd1
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-07-22 12:42:57 +1000

    modified local host port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  Rails.application.routes.default_url_options = { host: "http://localhost:4000" }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
