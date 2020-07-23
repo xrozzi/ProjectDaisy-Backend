fc5d8bf049a55ec5adb771e45a6b0811639fa626
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-07-23 10:22:27 +1000

    added create,update,destroy methods in images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,27 @@
 class ImagesController < ApplicationController
+    before_action :set_image, only: [:update, :destroy]
+
+    # PUT - create an image
+    def create 
+        upload = Cloudinary::Uploader.upload(params[:image])
+        image = Image.create(image: image["url"])
+        render json: item
+    end
+
+    # PATCH - updates an existing image
+    def update
+
+    end
+
+    # DELETE - deletes an image
+    def destroy
+        @image.Cloudinary::Uploader.destroy(id)
+    end
+
+    private
+
+        def set_image
+            @image = Message.find(params[:id])
+        end
+
 end
