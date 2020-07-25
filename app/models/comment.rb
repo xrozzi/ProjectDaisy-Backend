46153c8d96f799e3d66f9573f8bfed457d4263f6
esthercsoke <esthercsoke@gmail.com>
2020-07-26 00:27:10 +1000

    added model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :forum
+
+  validates :content, presence: true
+  accepts_nested_attributes_for :user
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
     Rails.application.routes.url_helpers
 
     has_secure_password
+
+    # validates :email, presence: true, 
+    # format: { with: /\A[^\.]([a-z0-9\.]){1,64}\@([a-z0-9\.]){1,255}/i, 
+    # message: "Invalid email address" }
+
+
     has_many :git_collaborations
     has_many :messages
     has_many :users_conversations
