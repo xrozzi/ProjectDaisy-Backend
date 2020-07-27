eb3b4743dbe7d32ef2da682abbb915992d45be8b
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-07-28 09:58:40 +1000

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,34 +15,6 @@ User.create(:email => 'admin1@projectdaisy.com.au', :password => 'secretpassword
 
 
 
-categories = [
-  { name: "Ruby"},
-  { name: "Python"},
-  { name: "JavaScript"},  
-  { name: "HTML/CSS"},
-  { name: "C++"},
-  { name: "React/MERN"},
-  { name: "Workplace Advice"},
-  { name: "Career Progression"},
-  { name: "For Fun/AmA"},
-  { name: "Show off your work"},
-]
-
-categories.each do |category|
-    Category.create(category)
-  end
-
-
-
-
-Forum.create(:title => "Category 5 post", discussion: "hihihi", user_id: 1, category_id: 5)
-Forum.create(:title => "Category 2 post", discussion: "hihihi", user_id: 2, category_id: 2)
-Forum.create(:title => "Category 9 post", discussion: "hellooooooooo", user_id: 2, category_id: 9)
-Forum.create(:title => "Category 9 post", discussion: "helllooooo", user_id: 2, category_id: 9)
-
-Comment.create(:body => "yeahhh i know so cool,", user_id: 1, forum_id: 9)
-Comment.create(:body => "yeahhh i know so cool,", user_id: 2, forum_id: 9)
-Comment.create(:body => "yeahhh i know so cool,", user_id: 1, forum_id: 2)
 
 
 
